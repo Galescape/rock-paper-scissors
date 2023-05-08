8ed196030425e381614d9a6d5b9e3f94a1be281c
Galescape <69803332+Galescape@users.noreply.github.com>
2023-05-08 16:51:18 +0200

    Add a function that plays a single round

--- javascript.js
+++ javascript.js
@@ -5,10 +5,27 @@ function getComputerChoice () {
     return finalChoice;
 }
 
-console.log(getComputerChoice());
-
 function formatPlayerInput (string) {
     let firstLetterCap = string[0].toUpperCase();
     let restOfString = string.slice(1).toLowerCase();
     return firstLetterCap + restOfString;
 }
+
+const playerChoice = formatPlayerInput(prompt(" "));
+const computerChoice = getComputerChoice();
+
+function singleRound (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `It's a tie! You both chose ${playerSelection}.`;
+    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        return `You lose! Computer chose ${computerSelection}, and ${computerSelection} beats ${playerSelection}.`;
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        return `You lose! Computer chose ${computerSelection}, and ${computerSelection} beats ${playerSelection}.`;
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return `You lose! Computer chose ${computerSelection}, and ${computerSelection} beats ${playerSelection}.`;
+    } else {
+        return `You win! ${playerSelection} beats ${computerSelection}.`
+    }
+}
+
+console.log(singleRound(playerChoice, computerChoice));
